0a3d6f07c0a1d803bc1ba887befc255cddccd4bd
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-07-17 17:18:36 +0100

    colour coded feed display

--- src/sciencemorningradio/gui/feed_display.py
+++ src/sciencemorningradio/gui/feed_display.py
@@ -1,9 +1,19 @@
 import toga
+from toga.style.pack import Pack
+
 import sciencemorningradio.playlists as playlists
+from sciencemorningradio.playlists.playlist import PlaylistError, PlaylistUpdating
+
 
 def display_feed(feed: playlists.Feed):
-    block = toga.Box()
+    if isinstance(feed.status, PlaylistError):
+        status_color = "red"
+    elif isinstance(feed.status, PlaylistUpdating):
+        status_color = "orange"
+    else:
+        status_color = "white"
 
+    block = toga.Box(style=Pack(padding=10, background_color=status_color))
     block.add(toga.Label(str(feed.feed_data)))
-    
+
     return block
