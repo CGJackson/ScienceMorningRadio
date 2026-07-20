2832ad87d9fe14fcb26e04dfa886d3297e229175
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-07-20 12:40:10 +0100

    Added checkboxes for which fields to read to new feed screen

--- src/sciencemorningradio/gui/new_feed.py
+++ src/sciencemorningradio/gui/new_feed.py
@@ -1,17 +1,30 @@
 import asyncio
 
 import toga
-from sciencemorningradio.arxiv_reader import Query,SearchFields,SortBy,SortDirection
+from sciencemorningradio.arxiv_reader import Query, SearchFields, SortBy
+from sciencemorningradio.article import Article
 from sciencemorningradio.playlists import Feed
 import sciencemorningradio.gui.main_screen as main_screen
 from toga.style.pack import COLUMN, Pack
 
 
 def build_new_feed_form(app):
-
     def create_new_feed(button: toga.Button):
-        query = Query(search={SearchFields.all: search_input.value},max_results=10,sort_by=SortBy.lastUpdatedDate)
-        feed = Feed(name=name_input.value, feed_data=query)
+        selected_attributes = tuple(
+            checkbox.text
+            for checkbox in attribute_checkboxes
+            if checkbox.value
+        )
+        query = Query(
+            search={SearchFields.all: search_input.value},
+            max_results=10,
+            sort_by=SortBy.lastUpdatedDate,
+        )
+        feed = Feed(
+            name=name_input.value,
+            feed_data=query,
+            read_attributes=selected_attributes,
+        )
         asyncio.create_task(feed.update())
         app.feed_list.append(feed)
         app.main_window.content = main_screen.run_screen(app)
@@ -21,6 +34,12 @@ def build_new_feed_form(app):
 
     search_label = toga.Label("Search")
     search_input = toga.TextInput()
+
+    attribute_label = toga.Label("Read attributes")
+    attribute_checkboxes = [
+        toga.Switch(text=attribute_name, value=attribute_name in {"title", "authors", "abstract"})
+        for attribute_name in Article.__dataclass_fields__.keys()
+    ]
 
     create_feed_button = toga.Button(
         "Create Feed",
@@ -33,7 +52,11 @@ def build_new_feed_form(app):
     form.add(name_input)
     form.add(search_label)
     form.add(search_input)
+    form.add(attribute_label)
+    for checkbox in attribute_checkboxes:
+        form.add(checkbox)
     form.add(create_feed_button)
 
     app.main_window.content = form
+    
 
